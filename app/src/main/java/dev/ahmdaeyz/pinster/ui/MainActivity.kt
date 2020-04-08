c702701983366f587bc93372d248588335079111
ahmdaeyz <ahmedaeyz@gmail.com>
2020-04-08 16:27:40 +0200

    refactor(filesystem): Moved MainActivity.kt to the ui package

--- app/src/main/java/dev/ahmdaeyz/pinster/ui/MainActivity.kt
+++ app/src/main/java/dev/ahmdaeyz/pinster/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.ahmdaeyz.pinster
+package dev.ahmdaeyz.pinster.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.ahmdaeyz.pinster.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
